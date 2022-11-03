c0b5b009e2fe2146d9d3ebaa9a2d3ff4c2aa0337
M Shoaib <mshoaib@Shazans-MacBook-Air.local>
2022-11-03 11:32:31 +0530

    nothing major

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-      url: "https://zomfoszsstzqszqfyoie.supabase.co",
-      anonKey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvbWZvc3pzc3R6cXN6cWZ5b2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU1OTU1OTksImV4cCI6MTk4MTE3MTU5OX0.f3qigEYXYgo8hawS3cGNa0KA8Gxb3iVliAR16rvuBII");
+      url: "ADD YOUR SUPABASE URL", anonKey: "ADD YOUR SUPABASE ANNON KEY");
   runApp(
     MultiProvider(
       providers: AppProviders.providers,
